b81472c82b5a41a23e30f851b4ebc76a58c764c6
DouglasSochC <3008474750101@ingenieria.usac.edu.gt>
2024-06-25 14:52:17 -0600

    [ADD] Perfil de usuario completado; [FIX] Navbar y session

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
 import { faUserCircle } from "@fortawesome/free-regular-svg-icons";
@@ -9,7 +9,7 @@ import { cerrarSesion, obtenerDatosUsuario } from "@/helpers/session";
 export default function NavbarComponent() {
 
   const router = useRouter();
-  const usuario = obtenerDatosUsuario();
+  const [usuario, setUsuario] = useState(null);
   const [notifications, setNotifications] = useState([
     {
       "id": 1,
@@ -61,6 +61,10 @@ export default function NavbarComponent() {
     );
   };
 
+  useEffect(() => {
+    setUsuario(obtenerDatosUsuario());
+  }, []);
+
   return (
     <Navbar variant="dark" expanded className="ps-0 pe-2 pb-0">
       <Container fluid className="px-0">
@@ -92,7 +96,7 @@ export default function NavbarComponent() {
                 <div className="media d-flex align-items-center">
                   <Image src="/user_icon.png" className="user-avatar md-avatar rounded-circle" />
                   <div className="media-body ms-2 text-dark align-items-center d-none d-lg-block">
-                    <span className="mb-0 font-small fw-bold">{usuario.nombre}</span>
+                    <span className="mb-0 font-small fw-bold">{usuario ? usuario.nombre : ''}</span>
                   </div>
                 </div>
               </Dropdown.Toggle>
