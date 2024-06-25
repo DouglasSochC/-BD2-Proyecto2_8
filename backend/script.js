6a9c77cd56803623feb38e60d45a2cd89a9737ea
Carlos Soto <3018554470101@ingenieria.usac.edu.gt>
2024-06-24 21:30:15 -0600

    Se agrego como crear un usuario admin

--- backend/script.js
+++ backend/script.js
@@ -1,15 +1,37 @@
-// script.js
-
 const mongoose = require('mongoose');
+const Usuario = require('./models/usuario'); // Importa el modelo de Usuario
 
 // Conectar a la base de datos
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/BookStore');
     console.log('MongoDB connected successfully');
+
+
+    // Verificar si ya existe un administrador
+    const adminExistente = await Usuario.findOne({ rol: 'Administrador' });
+    if (adminExistente) {
+      console.log('Ya existe un usuario administrador.');
+      return;
+    }
+
+    // Crear un nuevo usuario administrador
+    const nuevoAdmin = new Usuario({
+      nombre: 'Admin',
+      apellido: 'User',
+      edad: 30,
+      correoElectronico: 'admin@example.com',
+      contrasena: 'adminpassword', // Asegúrate de cambiar esto por una contraseña segura
+      rol: 'Administrador'
+    });
+    await nuevoAdmin.save();
+    console.log('Usuario administrador creado exitosamente.');
+
   } catch (error) {
     console.error('MongoDB connection error:', error);
     process.exit(1);
+  } finally {
+    mongoose.connection.close();
   }
 };
 
@@ -22,5 +44,5 @@ require('./models/compra');
 
 // Mensaje de éxito
 connectDB().then(() => {
-    console.log("Modelos cargados y base de datos conectada exitosamente.");
+  console.log("Modelos cargados y base de datos conectada exitosamente.");
 });
