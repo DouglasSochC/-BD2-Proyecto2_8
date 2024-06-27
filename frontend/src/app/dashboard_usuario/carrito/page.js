ee416cbc5fbc869b7c0bd9b1a5c8a2282e6f6fb8
Carlos Soto <3018554470101@ingenieria.usac.edu.gt>
2024-06-27 00:42:18 -0600

    Se modifico un poco el carrito en front

--- frontend/src/app/dashboard_usuario/carrito/page.js
+++ frontend/src/app/dashboard_usuario/carrito/page.js
@@ -1,24 +1,35 @@
 'use client'
 // components/Carrito.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { handleAxios, handleAxiosMultipart, handleAxiosError, handleAxiosMsg } from '@/helpers/axiosConfig';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
 const Carrito = () => {
-
-  // Datos simulados para el ejemplo
-  const [items, setItems] = useState([
-    { id: 1, name: 'Libro 1', price: 29.99, quantity: 1 },
-    { id: 2, name: 'Libro 2', price: 19.99, quantity: 2 },
-  ]);
-
-  // Estado para los detalles del checkout
+  const [items, setItems] = useState([]);
   const [direccionEnvio, setDireccionEnvio] = useState('123 Calle Falsa, Springfield');
   const [metodoPago, setMetodoPago] = useState('Efectivo');
   const [detallesPago, setDetallesPago] = useState('');
+  const [total, setTotal] = useState(0.00); // Estado para el total
+  const usuarioId = "667cbe44696b207f66f38e04"; // TODO: cambiar por el usuario real
 
-  // Calcular el total
-  const total = items.reduce((acc, item) => acc + item.price * item.quantity, 0);
+  // Función para obtener el pedido del carrito
+  const obtenerPedido = async () => {
+    try {
+      const response = await handleAxios().get(`/pedido/carrito/${usuarioId}`);
+      const data = response.data.data;
+      console.log("data: ", data);
+      setItems(data.pedido.libros); // Ajusta según la estructura real de tus datos
+      setTotal(data.pedido.total || 0.00); // Ajusta según los datos reales que regresen y establece 0.00 si no hay valor
+      setDireccionEnvio(data.direccionEnvio); // Ajusta según los datos reales que regresen
+    } catch (error) {
+      handleAxiosError(error);
+    }
+  };
+
+  useEffect(() => {
+    obtenerPedido();
+  }, []);
 
   // Manejar la eliminación de un producto
   const handleEliminar = (id) => {
@@ -33,7 +44,7 @@ const Carrito = () => {
   };
 
   // Manejar el envío del formulario de checkout
-  const handleCheckout = (e) => {
+  const handleCheckout = async (e) => {
     e.preventDefault();
     const pedido = {
       items,
@@ -45,9 +56,14 @@ const Carrito = () => {
       fechaPedido: new Date()
     };
     console.log('Pedido realizado:', pedido);
-    // Aquí podrías hacer una llamada a tu backend para guardar el pedido
-    // y luego redirigir al usuario a una página de confirmación o estado del pedido
-    router.push('/confirmacion');
+
+    try {
+      const response = await handleAxios().post('/api/pedido', pedido);
+      console.log('Pedido realizado:', response.data);
+      // Redirigir al usuario a una página de confirmación o estado del pedido
+    } catch (error) {
+      handleAxiosError(error);
+    }
   };
 
   return (
@@ -61,11 +77,11 @@ const Carrito = () => {
             <li key={item.id} className="list-group-item d-flex justify-content-between align-items-center">
               <div>
                 <h6 className="my-0">{item.name}</h6>
-                <small className="text-muted">Cantidad: {item.quantity}</small>
+                <small className="text-muted">Cantidad: {item.precio}</small>
               </div>
               <div className="d-flex align-items-center">
-                <span className="text-muted me-3">${(item.price * item.quantity).toFixed(2)}</span>
-                <button className="btn btn-danger btn-sm" onClick={() => handleEliminar(item.id)}>
+                <span className="text-muted me-3">${(item.cantidad * item.precio).toFixed(2)}</span>
+                <button className="btn btn-danger btn-sm" onClick={() => handleEliminar(item._id)}>
                   <FontAwesomeIcon icon={faTrashAlt} />
                 </button>
               </div>
